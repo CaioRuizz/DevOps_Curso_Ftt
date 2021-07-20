07bedd29b6dd791d1c9dd8bf70898706f66e3e2b
CaioRuizz <caio.ruiz00@gmail.com>
2021-07-20 20:04:04 -0300

    aplicação + container

---
+++ app/app.py
@@ -0,0 +1,18 @@
+"""Main application file"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/<random_string>')
+def returnBackwardsString(random_string):
+    """Reverse and return the provided URI"""
+    return "".join(reversed(random_string))
+
+
+@app.route('/')
+def index():
+    """Test http connection"""
+    return "App Funcionando"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
---
+++ tests/app_test.py
@@ -0,0 +1,17 @@
+from app import app
+
+import unittest
+
+class TestApp(unittest.TestCase):
+    """Unit tests defined for app.py"""
+
+    def test_return_backwards_string(self):
+        """Test return backwards simple string"""
+        random_string = "This is my test string"
+        random_string_reversed = "gnirts tset ym si sihT"
+        self.assertEqual(random_string_reversed, app.returnBackwardsString(random_string))
+    
+    def test_index(self):
+        """Test index"""
+        expected_return = "App Funcionando"
+        self.assertEqual(expected_return, app.index())
