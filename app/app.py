e8f2f78961cf14224c075b1959e63cefaa3a0e92
CaioRuizz <caio.ruiz00@gmail.com>
2021-07-21 20:17:27 -0300

    adicionado rota de bhaskara, funcoes matematicas e testes automatizados

--- app/app.py
+++ app/app.py
@@ -1,11 +1,29 @@
 """Main application file"""
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+@app.route('/bhaskara/<a>/<b>/<c>')
+def bhaskara(a, b, c):
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    x1 = divisao((-b + (((b ** 2) - (multiplicacao(multiplicacao(4, a), c))) ** (1/2))), (2 * a))
+    x2 = divisao((-b - (((b ** 2) - (multiplicacao(multiplicacao(4, a), c))) ** (1/2))), (2 * a))
+    return (str([x1, x2]))
+
+def multiplicacao(a, b):
+    return float(a) * float(b)
+
+def divisao(a, b):
+    return float(a) / float(b)
+
+def soma(a, b):
+    return float(a) + float(b)
 
 @app.route('/')
 def index():
     """Test http connection"""
-    return "Outro Deploy"
+    return "App Funcionando"
 
 
 if __name__ == '__main__':
--- tests/app_test.py
+++ tests/app_test.py
@@ -9,3 +9,27 @@ class TestApp(unittest.TestCase):
         """Test index"""
         expected_return = "App Funcionando"
         self.assertEqual(expected_return, app.index())
+
+    def test_bhaskara(self):
+        a = 4
+        b = -4
+        c = 1
+        self.assertEqual('[0.5, 0.5]', app.bhaskara(a, b, c))
+
+    def test_multiplicacao(self):
+        a = 5
+        b = 6
+        expected_result = 30
+        self.assertEqual(expected_result, app.multiplicacao(a,b))
+
+    def test_soma(self):
+        a = 5
+        b = 6
+        expected_result = 11
+        self.assertEqual(expected_result, app.soma(a,b))
+
+    def test_divisao(self):
+        a = 12
+        b = 6
+        expected_result = 2
+        self.assertEqual(expected_result, app.divisao(a,b))
