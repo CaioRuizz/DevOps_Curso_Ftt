fe090487f3aedb5b531be5574ad76f3e7b882d9f
CaioRuizz <caio.ruiz00@gmail.com>
2021-07-21 19:03:41 -0300

    removida funçao string reversa

--- app/app.py
+++ app/app.py
@@ -2,12 +2,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/<random_string>')
-def returnBackwardsString(random_string):
-    """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
-
-
 @app.route('/')
 def index():
     """Test http connection"""
--- tests/app_test.py
+++ tests/app_test.py
@@ -4,12 +4,6 @@ import unittest
 
 class TestApp(unittest.TestCase):
     """Unit tests defined for app.py"""
-
-    def test_return_backwards_string(self):
-        """Test return backwards simple string"""
-        random_string = "This is my test string"
-        random_string_reversed = "gnirts tset ym si sihT"
-        self.assertEqual(random_string_reversed, app.returnBackwardsString(random_string))
     
     def test_index(self):
         """Test index"""
