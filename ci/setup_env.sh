45c9e0a37a35ef5c9094d48c4a1fbc6a824d3289
CaioRuizz <caio.ruiz00@gmail.com>
2021-07-27 19:38:46 -0300

    segredos do github

---
+++ ci/setup_env.sh
@@ -0,0 +1,11 @@
+mkdir certs
+
+echo "$CA_PEM"
+
+echo "$CA_PEM" > certs/ca.pem
+echo "$CERT_PEM" > certs/cert.pem
+echo "$CONFIG_JSON" > certs/config.json
+echo "$ID_RSA" > certs/id_rsa
+echo "$ID_RSA_PUB" > certs/id_rsa.pub
+echo "$SERVER_KEY_PEM" > certs/server-key.pem
+echo "$SERVER_PEM" > certs/server.pem
