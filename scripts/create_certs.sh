89a6c40fb687609b2d7dced4f5a171553e8f74a6
CaioRuizz <caio.ruiz00@gmail.com>
2021-07-28 20:14:31 -0300

    corrigido chaves do docker

--- scripts/create_certs.sh
+++ scripts/create_certs.sh
@@ -1,12 +1,4 @@
 mkdir certs
 echo $CA | base64 -d > certs/ca.pem
-echo CA
-cat certs/ca.pem
 echo $CERT | base64 -d > certs/cert.pem
-echo ""
-echo cert
-cat certs/cert.pem
 echo $KEY | base64 -d > certs/key.pem
-echo ""
-echo key
-cat certs/key.pem
