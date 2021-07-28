0261e6b2958eedc4d72b0598a9aff32feb9f17c0
CaioRuizz <caio.ruiz00@gmail.com>
2021-07-28 20:01:27 -0300

    debug ci stage

--- scripts/create_certs.sh
+++ scripts/create_certs.sh
@@ -1,6 +1,7 @@
 mkdir certs
 echo $CA | base64 -d > certs/ca.pem
-
+cat certs/ca.pem
 echo $CERT | base64 -d > certs/cert.pem
-
+cat certs/cert.pem
 echo $KEY | base64 -d > certs/key.pem
+cat certs/key.pem
