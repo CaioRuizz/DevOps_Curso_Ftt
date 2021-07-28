56b43abac67c7d0a1b1bec88afd1649ae04d9699
CaioRuizz <caio.ruiz00@gmail.com>
2021-07-28 19:31:24 -0300

    removido cat certs

--- scripts/create_certs.sh
+++ scripts/create_certs.sh
@@ -1,7 +1,6 @@
 mkdir certs
 echo $CA | base64 -d > certs/ca.pem
-cat certs/ca.pem
+
 echo $CERT | base64 -d > certs/cert.pem
-cat certs/cert.pem
+
 echo $KEY | base64 -d > certs/key.pem
-cat certs/key.pem
